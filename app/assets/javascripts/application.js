b4c6a9e16510bbb40e5a4956b26936b1f0e23e35
danielmoralesp <daniel_moralesp@hotmail.com>
2019-04-05 20:18:15 -0500

    Ticket #1 - Linking CSS and JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,25 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+
+//= jquery/jquery-migrate.js
+//= jquery-ui/jquery-ui.min.js
+//= smoothscroll/SmoothScroll.min.js
+//= waypoints/waypoints.min.js
+//= counter-up/jquery.counterup.min.js
+//= datepicker/bootstrap-datepicker.min.js
+//= selectbox/jquery.selectbox-0.1.3.min.js
+//= owl-carousel/owl.carousel.min.js
+//= isotope/isotope.min.js
+//= fancybox/jquery.fancybox.min.js
+//= isotope/isotope-triger.min.js
+//= rateyo/jquery.rateyo.min.js
+//= map/js/markerclusterer.js
+//= map/js/rich-marker.js
+//= map/js/infobox_packed.js
+
+//= map.js
+//= app.js
+
 //= require_tree .
+
