8fa6ac3d5e7573fbc88ca2f61a6c7f4b58752a4c
danielmoralesp <daniel_moralesp@hotmail.com>
2019-04-09 18:34:07 -0500

    Ticket #1 - Gravatar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
+	before_action :avatar
+
+
   def index
   end
+
+
+  private
+  	def avatar
+  		@avatar = user_signed_in? ? current_user.avatar_url : nil
+  	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def avatar_url
+  	hash = Digest::MD5.hexdigest(email)
+  	"http://www.gravatar.com/avatar/#{hash}?s=25&d=identicon"
+  end
 end
