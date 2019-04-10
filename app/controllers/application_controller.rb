ad47d43c5d21f0f984f895eab85f7bb482914b6d
danielmoralesp <daniel_moralesp@hotmail.com>
2019-04-09 19:12:49 -0500

    Ticket #2 - Change Gravatar to Application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
 	## add parameters/column to Devise
 	before_action :add_parameters_to_user, if: :devise_controller?
+	before_action :avatar
 
 	protected
 
 		def add_parameters_to_user
 			devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
 		end
+
+		def avatar
+	  		@avatar = user_signed_in? ? current_user.avatar_url : nil
+	  	end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,4 @@
 class HomeController < ApplicationController
-	before_action :avatar
-
-
   def index
   end
-
-
-  private
-  	def avatar
-  		@avatar = user_signed_in? ? current_user.avatar_url : nil
-  	end
 end
