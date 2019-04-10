d8b378e9b3ddf8474d1d91592709cda0028f1f26
danielmoralesp <daniel_moralesp@hotmail.com>
2019-04-09 19:59:48 -0500

    Ticket #2 - Controller Security

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,17 @@
 class UsersController < ApplicationController
 	before_action :authenticate_user!
+	before_action :validate_user
 
   def show
+  	@user = User.find(params[:id])
   end
+
+
+  private
+  	def validate_user
+  		if current_user.id.to_s != params[:id]
+  			redirect_to root_path
+  			flash[:alert] = "You don't have permissions."
+  		end
+  	end
 end
